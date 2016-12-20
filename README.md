36c0143da5a4d44bd95c6cc84bc057b4898fc904
IgorSinyak <igorsinyak92@gmail.com>
2016-12-20 22:43:21 +0300

    'fix'

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # training-forms
+
+lessc less/main/main.less > css/main.css
--- script/script.js
+++ script/script.js
@@ -1,17 +1,18 @@
-var SELECTOR_ID = ('#selector')
-var SELECTOR = $(SELECTOR_ID);
-var SELECT_OPTIONS = $('#select-options');
+var SELECT_ID = ('#selector');
+var SELECT_ELEMENT = $(SELECT_ID);
+var SELECT_OPTIONS_ELEMENT = $('#select-options');
 var TITLE_LIST = $('.title');
-var HEADLINE_INPUT = $('#headline-input');
-var COUNTER = $('#counter')
+var HEADLINE_INPUT_ELEMENT = $('#headline-input');
+var COUNTER_ELEMENT = $('#counter');
+var HEADLINE_INPUT_MAXLENGTH = '/' + HEADLINE_INPUT_ELEMENT.attr('maxlength');
 
 var selectObj = {
   active: false,
   displayMenu: function() {
-    SELECT_OPTIONS.slideDown();
+    SELECT_OPTIONS_ELEMENT.slideDown();
     $(document).click(function(event) { //не уверен что правильно эту функцию расположил, но вроде работает, мгновенно сворачивает select если тыкнуть в любом месте
       var target = event.target;
-      if (!$(target).is(SELECTOR_ID) && !$(target).parents().is(SELECTOR_ID)) {
+      if (!$(target).is(SELECT_ID) && !$(target).parents().is(SELECT_ID)) {
         SELECT_OPTIONS.hide();
         selectObj.active = false;
       }
@@ -19,12 +20,12 @@ var selectObj = {
     selectObj.active = true;
   },
   hideMenu: function() {
-    SELECT_OPTIONS.slideUp();
+    SELECT_OPTIONS_ELEMENT.slideUp();
     selectObj.active = false;
   }
 }
 
-SELECTOR.click(function() {
+SELECT_ELEMENT.click(function() {
   if(selectObj.active) {
     selectObj.hideMenu();
   } else {
@@ -34,10 +35,10 @@ SELECTOR.click(function() {
 
 TITLE_LIST.click(function() {
   var title = $(this).html();
-  SELECTOR.children().html(title);
+  SELECT.children().html(title);
 });
 
-HEADLINE_INPUT.keyup(function() {
-  number = HEADLINE_INPUT.val().length;
-  COUNTER.html(number + '/40');
+HEADLINE_INPUT_ELEMENT.keyup(function() {
+  number = HEADLINE_INPUT_ELEMENT.val().length;
+  COUNTER_ELEMENT.html(number + HEADLINE_INPUT_MAXLENGTH);
 });
